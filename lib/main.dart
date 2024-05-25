50fe6db39c56ce0b9ffbb53df05301b46dcb64ed
Tamaki <83083937+tamaki380@users.noreply.github.com>
2024-05-26 02:55:54 +0900

    基本機能完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,218 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'dart:typed_data';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Audio Pitch Analyzer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? _filePath;
+  bool _processing = false;
+  Map<String, dynamic>? _result_json;
+  List<FlSpot> _pitchData = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+        // withData: true,
+        type: FileType.custom,
+        allowedExtensions: ['mp3']);
+    if (result != null && result.files.single.path != null) {
+      File file = File(result.files.single.path!);
+      int filesize = result.files.single.size;
+      // 拡張子が.mp3であるかを確認
+      if (!file.path.toLowerCase().endsWith('.mp3')) {
+        // .mp3でない場合はエラーメッセージを表示して終了
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Error'),
+            content: Text('Selected file must be in .mp3 format.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        );
+        return;
+      }
+      // 音声ファイルが1MB以下であることを確認
+      if (filesize != null) {
+        if (filesize > 1048576) {
+          // 1MB秒以上の場合はエラーメッセージを表示して終了
+          print('Debug message: Hello, world!' + filesize.toString());
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Error'),
+              content: Text('Selected audio file must be 1MB or less.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text('OK'),
+                ),
+              ],
+            ),
+          );
+          return;
+        }
+      } else {
+        return;
+      }
+      setState(() {
+        _filePath = file.path;
+        _processing = true;
+      });
+      Map<String, dynamic>? result_json = await _analyzePitch();
+      if (result_json == null) {
+        setState(() {
+          _processing = false;
+        });
+        return;
+      }
+      make_graph_data(result_json);
+      setState(() {
+        _result_json = result_json;
+        _processing = false;
+      });
+    }
+  }
+
+  // mp3データをバックエンドを送信して結果を受け取る
+  Future<Map<String, dynamic>?> _analyzePitch() async {
+    final uri = Uri.parse('http://127.0.0.1:5000/process');
+    final request = http.MultipartRequest('POST', uri);
+    request.headers.addAll({
+      'Content-Type': 'multipart/form-data',
     });
+    request.files.add(
+      await http.MultipartFile.fromPath(
+        'file', // サーバー側でファイルを受け取るフィールド名
+        _filePath!, // アップロードするファイルのパス
+        contentType: MediaType('audio', 'mpeg'), // ファイルのコンテンツタイプ
+      ),
+    );
+    final streamedResponse = await request.send(); //送信して待機
+    final response = await http.Response.fromStream(streamedResponse);
+
+    if (response.statusCode == 200) {
+      //通信が成功したときが200
+      return Future.value(jsonDecode(response.body));
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('sever error'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+      return null;
+    }
+  }
+
+  //返されたjsonからグラフデータを作成し保存
+  void make_graph_data(result_json) async {
+    String data_str = result_json!["result"];
+    List<String> data_list = data_str.split(",");
+    List<FlSpot> pitchdata = [];
+    data_list.asMap().forEach((index, value) {
+      double time = index.toDouble() * (1/26000);
+      double pitch = double.parse(value);
+      FlSpot spot = FlSpot(time, pitch);
+      pitchdata.add(spot);
+    });
+    setState(() {
+      _pitchData = pitchdata;
+    });
+    return;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter Audio Pitch Analyzer'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            ElevatedButton(
+              onPressed: _pickFile,
+              child: Text('Upload Audio File'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            SizedBox(height: 20),
+            _filePath != null
+                ? Text('File: $_filePath')
+                : Text('No file selected.'),
+            SizedBox(height: 20),
+            _processing == true
+                ? CircularProgressIndicator()
+                : _pitchData.isNotEmpty
+                    ? Container(
+                        height: 300,
+                        padding: EdgeInsets.all(16),
+                        child: LineChart(
+                          LineChartData(
+                            lineBarsData: [
+                              LineChartBarData(
+                                spots: _pitchData,
+                                isCurved: true,
+                                color: Colors.blue,
+                                barWidth: 2,
+                              )
+                            ],
+                            titlesData: FlTitlesData(
+                              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                              rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                              // leftTitles: AxisTitles(),
+                              bottomTitles: AxisTitles(
+                                sideTitles: SideTitles(
+                                  getTitlesWidget: (value, meta) => Text('${value.toInt().toString()}.00'),
+                                  showTitles: true,
+                                  interval: 1,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    : Text('No pitch data available.'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
